fb33e216ce24375530268e7569f219811cf9e8e4
cbyungmal <jgaines554@noscihigh.org>
2021-03-25 17:42:35 +0000

    complete

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
 //Create a variable called myObject and assign 
 //an object to it.
 
+var myObject = ();
+
 
 
 
 //Add two properties of firstName and lastName
 //using dot notation
-
+myObject.firstName = 'Jamar';
+myObject.lastName = 'Gaines';
 
 
 
 //Add two properties of hobbies and age 
 //using bracket notation
 //hobbies must be an array of at least 4 strings
-
+myObject = ['hobbies': 'gaming', 'sports']
+myObject = ['age': '16', '17']
 
 
 
 //Print the myObject variable
-
+myObject.print();
 
 
 
 //Use a for in loop to print all of the keys of the object
-
+var object = {
+  "key1": "object1"
+  "key2": "object2"
+  "key3": "object3"
+};
 
 
 
 ///Use a for in loop to print all of the values of the object
-
+var playstations = playstations[]{ps1, ps2, ps3, ps4, ps5};
 
 
 
@@ -39,3 +47,4 @@ create an object that is about a specific car
 Include: year, make, model, color and an array of features that you want your car to have
 
 */
+var car = {year: "2021", make: "toyota", model: "camry", color: "red";}
